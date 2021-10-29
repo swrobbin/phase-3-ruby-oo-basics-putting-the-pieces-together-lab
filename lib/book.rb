b0d410465c669da88d270981f5333d2407fbe55b
Stephen <swrobbin@gmail.com>
2021-10-29 17:45:49 -0400

    commit tree 379edfbd95926473da0992abf06f9b8c4cf8c15e

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,24 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    # def title
+    #     @title
+    # end  
+    
+    # def title=(title)
+    #     @title = title
+    # end    
 
+    def initialize(title)
+        # self.title=(title)
+        # object being initialize
+        @title = title
+        @page_count = 1
+    end 
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+        @page_count += 1
+    end   
+    
+end
 
